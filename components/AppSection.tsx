7c8bcba9556a073048ff2f12ef990086c6d8c3bc
Nicolas <72094122+magicniclus@users.noreply.github.com>
2025-12-08 17:21:59 +0100

    Update AppSection.tsx

--- components/AppSection.tsx
+++ components/AppSection.tsx
@@ -32,7 +32,7 @@ export default function AppSection() {
 
   return (
     <section className="py-16 lg:py-24 bg-white">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-12">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center">
           
           {/* Contenu à gauche */}
